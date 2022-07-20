3537cd3613c621b0d002cbdc98e2ea26f7d10f33
Coachjvweb <coach.jvweb@orange.fr>
2022-07-20 23:53:20 +0200

    Initialisation des pages d'accueil

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+require "cnx.php" ;
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Administration élève AKFSD</title>
+</head>
+
+<body>
+	<h1>Bienvenu Joël VALENTIN</h1>
+	<br/>
+	<p>Ce site est fait pour vous !<br/>
+	je dois finalement prendre mes affaires !
+		- Sur la barre de menu, on aura : Prénom NOM - Grade - Couleur grade - Reste à payer : prix €<br/>
+		- Cadre pour infos interne du club<br/>
+		- Cadre pour les stages (Inscription, Réservation, Paiement, Annulation)<br/>
+		- Cadre pour les passages de grade<br/>
+		- Bandeau vertical pour les différents niveaux des différents styles<br/>
+		- Bandeau vertical pour proposition de formation<br/>
+		- Dépôt possible des devoirs pour passge de grade<br/>
+		- Possibilité de créer une cagnote pour petit achat<br/>
+		- Possibilité pour coaching personnel payant<br/>
+		- Quelques vidéos à mettre lignes<br/>
+		- Possibilité de modifier ses données<br/>
+		- Possibilité de commander pour les élèves (Tenue, Chaussure, Armes, Accéssoires)<br/>
+		- Afficher numéro de licence<br/>
+		- Afficher numéro d'affiliation<br/>
+		- Préparation au examen<br/>
+		
+		<form>
+			<h2>Se connecter</h2></h1>
+			Indentifiant : <input name="firstname" value="" /><br/>
+			Mot de passe : <input name="pwd" value="" /><br/>
+			
+	
+	
+		</form>
+	</p>
+</body>
+</html>
